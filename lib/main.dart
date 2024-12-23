1bda3444be4832ba7314900f9fe901ee27892d1f
Avinash Magar <avinashmagar15@gmail.com>
2024-12-23 14:29:30 +0530

    new code

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_course/start_screen.dart';
 
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StartScreen(),
+    ),
+  );
+}
