657c9b380c7a3ef58b427e0a10fb33eb0558cc87
Avinash Magar <avinashmagar15@gmail.com>
2025-01-11 13:40:08 +0530

    new changes

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -13,7 +13,17 @@ class AnswerButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onTap,
-      style: ElevatedButton.styleFrom(),
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 40,
+          vertical: 10,
+        ),
+        backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+      ),
       child: Text(answeText),
     );
   }
